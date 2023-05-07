74598aa9f3bdd362a1f8cce2447eb1a3d48ae127
Mohammad Jabir <mohammadj94@gmail.com>
2023-05-07 13:50:55 -0600

    review complete.

--- IntVariables/Program.cs
+++ IntVariables/Program.cs
@@ -3,3 +3,18 @@
 // no - 1.2, -5.7
 
 int age = 0;
+age = 43;
+
+int ageInTenYears = age + 10;
+
+// two billion +/-
+// signed Int32
+// unsigned - four billion 
+// bit - 0 or 1
+// byte - 8 bits - 00000000
+// 1, 11, 111
+
+Console.WriteLine(age);
+Console.WriteLine(ageInTenYears);
+
+// don't divide ints - Console.WriteLine(age / 2);
